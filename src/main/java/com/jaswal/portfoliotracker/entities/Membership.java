09568264acf904c9c7e55c51a83f23c2f95cd373
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2026-02-04 16:34:10 -0700

    Membership Entity complete

---
+++ src/main/java/com/jaswal/portfoliotracker/entities/Membership.java
@@ -0,0 +1,30 @@
+package com.jaswal.portfoliotracker.entities;
+
+import com.jaswal.portfoliotracker.enums.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "membership",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "portfolio_id"}))
+public class Membership {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long membershipId;
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne()
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
---
+++ src/main/java/com/jaswal/portfoliotracker/enums/Role.java
@@ -0,0 +1,7 @@
+package com.jaswal.portfoliotracker.enums;
+
+public enum Role {
+    ADMIN,
+    MEMBER,
+    VISITOR
+}
