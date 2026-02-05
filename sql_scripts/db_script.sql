36288aeb3af05ab0b8ab73636b272a7f5883fefd
KulvanshJaswal <kulvansh.jaswal@ucalgary.ca>
2026-02-04 17:10:22 -0700

    Modify database schema for consistency and clarity
    Updated ENUM values to uppercase and changed data types for price and cost fields.

--- sql_scripts/db_script.sql
+++ sql_scripts/db_script.sql
@@ -1,4 +1,6 @@
+DROP DATABASE IF EXISTS portfolio_tracker;
 CREATE DATABASE portfolio_tracker;
+USE portfolio_tracker;
 
 CREATE TABLE users(
 	user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
@@ -19,7 +21,7 @@ CREATE TABLE membership(
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
 	portfolio_id BIGINT,
 	FOREIGN KEY (portfolio_id) REFERENCES portfolios(portfolio_id),
-	role ENUM('admin', 'member', 'visitor') NOT NULL,
+	role ENUM('ADMIN', 'MEMBER', 'VISITOR') NOT NULL,
 	UNIQUE(portfolio_id, user_id)
 );
 
@@ -27,11 +29,11 @@ CREATE TABLE transactions(
 	transaction_id BIGINT PRIMARY KEY AUTO_INCREMENT,
 	portfolio_id BIGINT NOT NULL,
 	FOREIGN KEY (portfolio_id) REFERENCES portfolios(portfolio_id),
-	transaction_type ENUM('buy', 'sell', 'withdrawal', 'deposit') NOT NULL,
 	symbol VARCHAR(10) NOT NULL,
-	asset_type ENUM('stock', 'crypto', 'cash'),
+	transaction_type ENUM('BUY', 'SELL', 'WITHDRAWAL', 'DEPOSIT') NOT NULL,
+	asset_type ENUM('STOCK', 'CRYPTO', 'CASH') NOT NULL,
 	quantity DECIMAL(18, 8) NOT NULL,
-	price_per_unit BIGINT NOT NULL,
+	price_per_unit DECIMAL(18,8) NOT NULL,
 	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -40,16 +42,16 @@ CREATE TABLE positions (
     portfolio_id BIGINT NOT NULL,
     FOREIGN KEY (portfolio_id) REFERENCES portfolios(portfolio_id),
     symbol VARCHAR(10) NOT NULL,
-    asset_type ENUM('stock', 'crypto', 'cash') NOT NULL,
+    asset_type ENUM('STOCK', 'CRYPTO', 'CASH') NOT NULL,
     total_quantity DECIMAL(18, 8) NOT NULL,
-    total_cost BIGINT NOT NULL,
-    average_cost_per_unit BIGINT NOT NULL,
+    total_cost DECIMAL(18,8) NOT NULL,
+    average_cost_per_unit DECIMAL(18,8) NOT NULL,
     UNIQUE(portfolio_id, symbol)
 );
 
 CREATE TABLE price_quotes (
 	symbol VARCHAR(10) PRIMARY KEY,
-    price BIGINT NOT NULL,
+    price DECIMAL(18,8) NOT NULL,
     last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     source VARCHAR(50) NOT NULL
 );
@@ -59,7 +61,7 @@ CREATE TABLE invites (
     portfolio_id BIGINT NOT NULL,
     invite_code VARCHAR(50) UNIQUE NOT NULL,
     created_by BIGINT NOT NULL,
-    role ENUM('admin', 'member', 'visitor'),
+    role ENUM('ADMIN', 'MEMBER', 'VISITOR'),
     max_uses INT NOT NULL,
     current_uses INT DEFAULT 0,
     expires_at TIMESTAMP NOT NULL,
@@ -67,4 +69,3 @@ CREATE TABLE invites (
     FOREIGN KEY (portfolio_id) REFERENCES portfolios(portfolio_id),
     FOREIGN KEY (created_by) REFERENCES users(user_id)
 );
-
