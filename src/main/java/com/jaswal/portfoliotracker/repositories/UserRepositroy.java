7a806327f482a9919b02b0a31b70b734dd67f6e3
KJ597 <Jaswalkuljot@gmail.com>
2026-02-07 18:26:19 -0700

    Finished the UserRepositroy

---
+++ src/main/java/com/jaswal/portfoliotracker/repositories/UserRepositroy.java
@@ -0,0 +1,17 @@
+package com.jaswal.portfoliotracker.repositories;
+
+
+import com.jaswal.portfoliotracker.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+
+public interface UserRepositroy extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
+
+
