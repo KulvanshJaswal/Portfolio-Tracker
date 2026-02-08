bfbccfe8621c42848afe758b2a5061d1ab4b1be2
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2026-02-07 19:16:47 -0700

    Membership Basic Repository Done

--- src/main/java/com/jaswal/portfoliotracker/repositories/InviteRepository.java
+++ src/main/java/com/jaswal/portfoliotracker/repositories/InviteRepository.java
@@ -2,11 +2,13 @@ package com.jaswal.portfoliotracker.repositories;
 
 import com.jaswal.portfoliotracker.entities.Invite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface InviteRepository extends JpaRepository<Invite, Long> {
 
     Optional<Invite> findByInviteCode(String inviteCode);
