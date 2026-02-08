5be7e4981084a6bfdb893f5c07a1e015b4c05c01
KJ597 <Jaswalkuljot@gmail.com>
2026-02-07 19:14:49 -0700

    Finished the TransactionRepository

---
+++ src/main/java/com/jaswal/portfoliotracker/repositories/TransactionRepository.java
@@ -0,0 +1,37 @@
+package com.jaswal.portfoliotracker.repositories;
+
+import com.jaswal.portfoliotracker.entities.Portfolio;
+import com.jaswal.portfoliotracker.entities.Transaction;
+import com.jaswal.portfoliotracker.enums.AssetType;
+import com.jaswal.portfoliotracker.enums.TransactionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByPortfolio(Portfolio portfolio);
+
+    List<Transaction> findBySymbol(String symbol);
+
+    List<Transaction> findByTransactionType(TransactionType transactionType);
+
+    List<Transaction> findBySymbolAndPortfolio(String symbol, Portfolio portfolio);
+
+
+    List<Transaction> findBySymbolAndTransactionType(String symbol, TransactionType transactionType);
+
+
+    List<Transaction> findByAssetTypeAndPortfolio(AssetType assetType, Portfolio portfolio);
+
+
+
+
+
+
+
+}
