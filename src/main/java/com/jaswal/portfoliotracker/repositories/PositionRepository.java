175cb82a7560a7599fcc0c3d93e155aa959e9f0e
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2026-02-10 18:53:03 -0700

    Position Repo complete

---
+++ src/main/java/com/jaswal/portfoliotracker/repositories/PositionRepository.java
@@ -0,0 +1,20 @@
+package com.jaswal.portfoliotracker.repositories;
+
+import com.jaswal.portfoliotracker.entities.Position;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PositionRepository extends JpaRepository<Position, Long> {
+
+    Optional<Position> findByPortfolio_PortfolioIdAndSymbol(Long portfolioId, String symbol);
+
+    List<Position> findByPortfolio_PortfolioId(Long portfolioId);
+
+    List<Position> findByPortfolio_PortfolioId(Long portfolioId, Sort sort);
+
+}
