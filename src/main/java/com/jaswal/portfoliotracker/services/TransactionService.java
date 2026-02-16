c6a01a35a83bbf289136a254eb09c86c0fede5f5
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2026-02-16 12:01:35 -0700

    withdrawal() now done in transaction service

--- src/main/java/com/jaswal/portfoliotracker/services/TransactionService.java
+++ src/main/java/com/jaswal/portfoliotracker/services/TransactionService.java
@@ -31,7 +31,8 @@ public class TransactionService {
             throw new IllegalArgumentException("The amount being deposited has to be greater than zero");
         }
         Portfolio portfolio = portfolioRepository.findById(portfolioId).orElseThrow(
-                () -> new IllegalArgumentException("Portfolio does not exist."));
+                () -> new IllegalArgumentException("Portfolio does not exist.")
+        );
 
         //Make Transaction Record
         Transaction transaction = new Transaction();
@@ -48,7 +49,7 @@ public class TransactionService {
         Position position = positionRepository.findByPortfolio_PortfolioIdAndSymbol(
                 portfolioId, "CASH").orElse(new Position());
         if (position.getPositionId()== null) {
-            // it's new — set up all the fields
+            //set up all the fields
             position.setPortfolio(portfolio);
             position.setSymbol("CASH");
             position.setAssetType(AssetType.CASH);
@@ -57,12 +58,45 @@ public class TransactionService {
             position.setAverageCostPerUnit(BigDecimal.ONE);
         }
         else {
-            // it exists — just add to the quantity
+            //just add to the quantity
             position.setTotalQuantity(position.getTotalQuantity().add(amount));
             position.setTotalCost(position.getTotalCost().add(amount));
         }
         positionRepository.save(position);
     }
 
+    public void withdrawal(Long portfolioId, BigDecimal amount){
+        //Initial error checking
+        if(amount.compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException("The amount being withdraw must be greater than zero");
+        }
+        Portfolio portfolio = portfolioRepository.findById(portfolioId).orElseThrow(
+                () -> new IllegalArgumentException("Portfolio does not exist")
+        );
+        //Checking cash
+        Position position = positionRepository.findByPortfolio_PortfolioIdAndSymbol(portfolioId, "CASH").orElseThrow(
+                () -> new IllegalArgumentException("Do not have sufficient funds")
+        );
+        BigDecimal userAmount = position.getTotalQuantity();
+        if(userAmount.compareTo(amount) < 0){
+            throw new IllegalArgumentException("Do not have sufficient funds");
+        }
+
+        //Have sufficient - edit position
+        position.setTotalCost(position.getTotalCost().subtract(amount));
+        position.setTotalQuantity(position.getTotalQuantity().subtract(amount));
+        positionRepository.save(position);
+
+        Transaction transaction = new Transaction();
+        transaction.setTransactionType(TransactionType.WITHDRAWAL);
+        transaction.setAssetType(AssetType.CASH);
+        transaction.setPortfolio(portfolio);
+        transaction.setQuantity(amount);
+        transaction.setSymbol("CASH");
+        transaction.setPricePerUnit(BigDecimal.ONE);
+        transaction.setTimestamp(LocalDateTime.now());
+        transactionRepository.save(transaction);
+
+    }
 
 }
