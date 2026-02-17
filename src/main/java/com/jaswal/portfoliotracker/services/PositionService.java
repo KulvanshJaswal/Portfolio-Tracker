38626a3c396330db3be447505fa67e9bcf9ea353
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2026-02-17 11:15:43 -0700

    Position service class made

---
+++ src/main/java/com/jaswal/portfoliotracker/services/PositionService.java
@@ -0,0 +1,10 @@
+package com.jaswal.portfoliotracker.services;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
+public class PositionService {
+
+}
