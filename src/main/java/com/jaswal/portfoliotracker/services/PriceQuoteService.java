f0e6311cfc6311e12b2ba8dfd9e54ab9840980f5
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2026-02-19 15:42:53 -0700

    price quote service method getprice is done

--- src/main/java/com/jaswal/portfoliotracker/services/PriceQuoteService.java
+++ src/main/java/com/jaswal/portfoliotracker/services/PriceQuoteService.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import tools.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Map;
 @Service
 @Transactional
@@ -29,21 +29,31 @@ public class PriceQuoteService {
     public BigDecimal getPrice(String symbol){
         //Initial error checking
         PriceQuote priceQuote = priceQuoteRepository.findById(symbol).orElse(new PriceQuote());
-
         String response = restClient.get()
                 .uri("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + apiKey)
                 .retrieve()
                 .body(String.class);
 
+        //Fetching price
         ObjectMapper objectMapper = new ObjectMapper();
+        BigDecimal price;
         try {
             Map<String, Object> jsonMap = objectMapper.readValue(response, Map.class);
             Map<String, Object> globalQuote = (Map<String, Object>) jsonMap.get("Global Quote");
             String priceString = (String) globalQuote.get("05. price");
-            BigDecimal price = new BigDecimal(priceString);
-        } catch (JsonProcessingException e) {
+            price = new BigDecimal(priceString);
+        } catch (Exception e) {
             throw new RuntimeException("Failed to parse price data", e);
         }
+        //Setting new priceQuote object if not exists
+        if(priceQuote.getSymbol() == null){
+            priceQuote.setSource("Alpha Vantage");
+            priceQuote.setSymbol(symbol);
+        }
+        priceQuote.setPrice(price);
+        priceQuote.setLastUpdated(LocalDateTime.now());
+        priceQuoteRepository.save(priceQuote);
 
+        return price;
     }
 }
