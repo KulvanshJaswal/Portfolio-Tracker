8a808cb52d115c2500865d383daf3a470cbd845c
KJ597 <Jaswalkuljot@gmail.com>
2026-02-18 18:23:36 -0700

    Done the PortfolioService

--- src/main/java/com/jaswal/portfoliotracker/repositories/PortfolioRepository.java
+++ src/main/java/com/jaswal/portfoliotracker/repositories/PortfolioRepository.java
@@ -4,11 +4,14 @@ import com.jaswal.portfoliotracker.entities.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 
     Optional<Portfolio> findByCreatedBy_UserIdAndName(Long userId, String name);
+
+    List<Portfolio> findByCreatedBy_UserId(Long userId);
 
 }
